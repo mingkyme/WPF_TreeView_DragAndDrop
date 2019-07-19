b642c54346f4fcb154b048897fa1c62cc0e01601
민기 김 <mgk9718@gmail.com>
2019-07-19 13:28:34 +0900

    3 단계

--- DragAndDrop/MainWindow.xaml.cs
+++ DragAndDrop/MainWindow.xaml.cs
@@ -21,61 +21,90 @@ namespace DragAndDrop
         public MainWindow()
         {
             InitializeComponent();
+            LastChildren(XAML_TreeView);
+
 
         }
-        private bool isDrag;
-        private void XAML_TreeView_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        private void LastChildren(ItemsControl control)
         {
-            isDrag = true;
-        }
-
-        private void XAML_TreeView_PreviewMouseMove(object sender, MouseEventArgs e)
-        {
-            if(isDrag)
+            if(control.Items.Count == 0)
             {
-
-
+                control.PreviewDragOver += TreeViewItem_PreviewDragOver;
+                control.PreviewDrop += TreeViewItem_PreviewDrop;
+                control.PreviewMouseLeftButtonDown += XAML_TreeView_PreviewMouseLeftButtonDown;
+                control.PreviewDragLeave += TreeViewItem_DragLeave;
+                control.PreviewMouseMove += TreeViewItem_PreviewMouseMove;
+            }
+            else
+            {
+                foreach (ItemsControl i in control.Items)
+                {
+                    LastChildren(i);
+                }
             }
         }
-
-        private void XAML_TreeView_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            isDrag = false;
-            Console.WriteLine("OUTPUT");
-            try
-            {
-                focusedTreeViewItem.FontWeight = FontWeights.Normal;
-                focusedTreeViewItem = null;
-            }
-            catch
-            {
-
-            }
-            
-        }
-
-
+        
+        TreeViewItem selectedFirstTreeViewItem;
+        Point mousePos;
         private void XAML_TreeView_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
+            selectedFirstTreeViewItem = sender as TreeViewItem;
+
+            Console.WriteLine("SELECT {0}", selectedFirstTreeViewItem.Header);
+            selectedFirstTreeViewItem.IsSelected = true;
+            mousePos = Mouse.GetPosition(null);
+            
 
         }
-        TreeViewItem focusedTreeViewItem = null;
-        private void XAML_TreeView_DragOver(object sender, DragEventArgs e)
+
+        private void TreeViewItem_PreviewDragOver(object sender, DragEventArgs e)
         {
-            try
+            // 드래그 될 위치를 강조
+            (sender as TreeViewItem).FontWeight = FontWeights.UltraBold;
+        }
+        // 교환
+        private void TreeViewItem_PreviewDrop(object sender, DragEventArgs e)
+        {
+            Console.WriteLine("DROP {0}", (sender as TreeViewItem).Header);
+            Console.WriteLine("Change {0} to {1}", selectedFirstTreeViewItem.Header, (sender as TreeViewItem).Header);
+            if( !sender.Equals(selectedFirstTreeViewItem))
             {
-                if(focusedTreeViewItem != null)
-                {
-                    focusedTreeViewItem.FontWeight = FontWeights.Normal;
-                }
-                (e.Source as TreeViewItem).FontWeight = FontWeights.UltraBold;
-                focusedTreeViewItem = e.Source as TreeViewItem;
-            }
-            catch
-            {
+                var parent = (sender as TreeViewItem).Parent as ItemsControl;
+
+                int index = parent.Items.IndexOf(sender);
+                (selectedFirstTreeViewItem.Parent as ItemsControl).Items.Remove(selectedFirstTreeViewItem);
+                parent.Items.Insert(index, selectedFirstTreeViewItem);
+
 
             }
-            
+            (sender as TreeViewItem).FontWeight = FontWeights.Normal;
+            (XAML_TreeView.Items[0] as TreeViewItem).IsSelected = false;
+            selectedFirstTreeViewItem = null;
+
+
+
+        }
+
+
+        private void TreeViewItem_DragLeave(object sender, DragEventArgs e)
+        {
+            (sender as TreeViewItem).FontWeight = FontWeights.Normal;
+
+        }
+
+        private void TreeViewItem_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            // 일정량 만큼 움직였을 때 드래그 시작
+            int distance = 3;
+            if (selectedFirstTreeViewItem != null)
+            {
+                if( ((Mouse.GetPosition(null).X - mousePos.X) > distance || (Mouse.GetPosition(null).X - mousePos.X) < -distance) ||
+                    ((Mouse.GetPosition(null).Y - mousePos.Y) > distance || (Mouse.GetPosition(null).Y - mousePos.Y) < -distance))
+                {
+                    DragDrop.DoDragDrop(selectedFirstTreeViewItem, selectedFirstTreeViewItem, DragDropEffects.Move);
+                }
+                
+            }
         }
     }
 }
