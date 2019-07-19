f5b87511724b2224d82db3b15bb80327de0087c7
민기 김 <mgk9718@gmail.com>
2019-07-19 09:25:15 +0900

    2 단계

--- DragAndDrop/MainWindow.xaml.cs
+++ DragAndDrop/MainWindow.xaml.cs
@@ -17,195 +17,65 @@ namespace DragAndDrop
 {
     public partial class MainWindow : Window
     {
-        Point _lastMouseDown;
-        TreeViewItem draggedItem, _target;
 
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+        private bool isDrag;
+        private void XAML_TreeView_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            isDrag = true;
         }
 
-        private void TreeView_MouseDown
-     (object sender, MouseButtonEventArgs e)
+        private void XAML_TreeView_PreviewMouseMove(object sender, MouseEventArgs e)
         {
-            if (e.ChangedButton == MouseButton.Left)
+            if(isDrag)
             {
-                _lastMouseDown = e.GetPosition(XAML_TreeView);
+
+
             }
         }
-        private void treeView_MouseMove(object sender, MouseEventArgs e)
+
+        private void XAML_TreeView_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            isDrag = false;
+            Console.WriteLine("OUTPUT");
+            try
+            {
+                focusedTreeViewItem.FontWeight = FontWeights.Normal;
+                focusedTreeViewItem = null;
+            }
+            catch
+            {
+
+            }
+            
+        }
+
+
+        private void XAML_TreeView_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+        TreeViewItem focusedTreeViewItem = null;
+        private void XAML_TreeView_DragOver(object sender, DragEventArgs e)
         {
             try
             {
-                if (e.LeftButton == MouseButtonState.Pressed)
+                if(focusedTreeViewItem != null)
                 {
-                    Point currentPosition = e.GetPosition(XAML_TreeView);
-
-                    if ((Math.Abs(currentPosition.X - _lastMouseDown.X) > 10.0) ||
-                        (Math.Abs(currentPosition.Y - _lastMouseDown.Y) > 10.0))
-                    {
-                        draggedItem = (TreeViewItem)XAML_TreeView.SelectedItem;
-                        if (draggedItem != null)
-                        {
-                            DragDropEffects finalDropEffect =
-                DragDrop.DoDragDrop(XAML_TreeView,
-                    XAML_TreeView.SelectedValue,
-                                DragDropEffects.Move);
-                            //Checking target is not null and item is
-                            //dragging(moving)
-                            if ((finalDropEffect == DragDropEffects.Move) &&
-                    (_target != null))
-                            {
-                                // A Move drop was accepted
-                                if (!draggedItem.Header.ToString().Equals
-                    (_target.Header.ToString()))
-                                {
-                                    CopyItem(draggedItem, _target);
-                                    _target = null;
-                                    draggedItem = null;
-                                }
-                            }
-                        }
-                    }
+                    focusedTreeViewItem.FontWeight = FontWeights.Normal;
                 }
+                (e.Source as TreeViewItem).FontWeight = FontWeights.UltraBold;
+                focusedTreeViewItem = e.Source as TreeViewItem;
             }
-            catch (Exception)
+            catch
             {
-            }
-        }
-        private void treeView_DragOver(object sender, DragEventArgs e)
-        {
-            try
-            {
-                Point currentPosition = e.GetPosition(XAML_TreeView);
 
-                if ((Math.Abs(currentPosition.X - _lastMouseDown.X) > 10.0) ||
-                   (Math.Abs(currentPosition.Y - _lastMouseDown.Y) > 10.0))
-                {
-                    // Verify that this is a valid drop and then store the drop target
-                    TreeViewItem item = GetNearestContainer
-                    (e.OriginalSource as UIElement);
-                    if (CheckDropTarget(draggedItem, item))
-                    {
-                        e.Effects = DragDropEffects.Move;
-                    }
-                    else
-                    {
-                        e.Effects = DragDropEffects.None;
-                    }
-                }
-                e.Handled = true;
             }
-            catch (Exception)
-            {
-            }
-        }
-        private void treeView_Drop(object sender, DragEventArgs e)
-        {
-            try
-            {
-                e.Effects = DragDropEffects.None;
-                e.Handled = true;
-
-                // Verify that this is a valid drop and then store the drop target
-                TreeViewItem TargetItem = GetNearestContainer
-                    (e.OriginalSource as UIElement);
-                if (TargetItem != null && draggedItem != null)
-                {
-                    _target = TargetItem;
-                    e.Effects = DragDropEffects.Move;
-                }
-            }
-            catch (Exception)
-            {
-            }
-        }
-        private TreeViewItem GetNearestContainer(UIElement element)
-        {
-            // Walk up the element tree to the nearest tree view item.
-            TreeViewItem container = element as TreeViewItem;
-            while ((container == null) && (element != null))
-            {
-                element = VisualTreeHelper.GetParent(element) as UIElement;
-                container = element as TreeViewItem;
-            }
-            return container;
-        }
-        private bool CheckDropTarget(TreeViewItem _sourceItem, TreeViewItem _targetItem)
-        {
-            //Check whether the target item is meeting your condition
-            bool _isEqual = false;
-            if (!_sourceItem.Header.ToString().Equals(_targetItem.Header.ToString()))
-            {
-                _isEqual = true;
-            }
-            return _isEqual;
-
-        }
-        private void CopyItem(TreeViewItem _sourceItem, TreeViewItem _targetItem)
-        {
-
-            //Asking user wether he want to drop the dragged TreeViewItem here or not
-            if (MessageBox.Show("Would you like to drop " + _sourceItem.Header.ToString() + " into " + _targetItem.Header.ToString() + "", "", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
-            {
-                try
-                {
-                    //adding dragged TreeViewItem in target TreeViewItem
-                    addChild(_sourceItem, _targetItem);
-
-                    //finding Parent TreeViewItem of dragged TreeViewItem 
-                    TreeViewItem ParentItem = FindVisualParent<TreeViewItem>(_sourceItem);
-                    // if parent is null then remove from TreeView else remove from Parent TreeViewItem
-                    if (ParentItem == null)
-                    {
-                        XAML_TreeView.Items.Remove(_sourceItem);
-                    }
-                    else
-                    {
-                        ParentItem.Items.Remove(_sourceItem);
-                    }
-                }
-                catch
-                {
-
-                }
-            }
-
-        }
-        public void addChild(TreeViewItem _sourceItem, TreeViewItem _targetItem)
-        {
-            // add item in target TreeViewItem 
-            TreeViewItem item1 = new TreeViewItem();
-            item1.Header = _sourceItem.Header;
-            _targetItem.Items.Add(item1);
-            foreach (TreeViewItem item in _sourceItem.Items)
-            {
-                addChild(item, item1);
-            }
-        }
-        static TObject FindVisualParent<TObject>(UIElement child) where TObject : UIElement
-        {
-            if (child == null)
-            {
-                return null;
-            }
-
-            UIElement parent = VisualTreeHelper.GetParent(child) as UIElement;
-
-            while (parent != null)
-            {
-                TObject found = parent as TObject;
-                if (found != null)
-                {
-                    return found;
-                }
-                else
-                {
-                    parent = VisualTreeHelper.GetParent(parent) as UIElement;
-                }
-            }
-
-            return null;
+            
         }
     }
 }
